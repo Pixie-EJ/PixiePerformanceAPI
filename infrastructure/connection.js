1761ef27ebd19a9b7a21098e756467f217ac18ae
DESKTOP-Q87ITDP\pablo <20007223@uepg.br>
2021-12-14 18:35:13 -0300

    fix: database settings

--- infrastructure/connection.js
+++ infrastructure/connection.js
@@ -1,11 +1,10 @@
-//const mysql = require('mysql');
+const mariadb = require('mariadb');
 
-//const connection = mysql.createConnection({
-//    host: 'localhost',
-//    port: 3306,
-//    user: 'root',
-//    password: '',
-//    database:'fpope'
-//});
-//
-//module.exports = connection;
+const connection = mariadb.createPool({
+    host: 'HOSTNAME', 
+    user:'USER', 
+    password: 'PASSWORD',
+    connectionLimit: '?',
+});
+
+module.exports = connection;
